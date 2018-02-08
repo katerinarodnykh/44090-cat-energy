f8f3f7e9ed730390feb7d3969f9f41af1a2fd3da
Katerina Rodnykh <katerina.rodnykh@yandex.ru>
2018-02-08 20:13:26 +0300

    Добавление карты по API

---
+++ source/js/map.js
@@ -0,0 +1,14 @@
+function initMap() {
+  var uluru = {lat: 59.938955, lng: 30.323115};
+  var mapCenter = {lat: 59.938948, lng: 30.319358};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: mapCenter
+  });
+  image = 'img/map-pin.png'
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: image
+  });
+}
