86a5828e445e371a19a4965656e360cf09b81186
Katerina Rodnykh <katerina.rodnykh@yandex.ru>
2018-02-15 03:43:43 +0300

    Фоновые изображения для карты, js для положения маркера на десктопе

--- source/js/map.js
+++ source/js/map.js
@@ -1,8 +1,11 @@
+var uluru = {lat: 59.938955, lng: 30.323115};
+var desktopCenter = {lat: 59.939095, lng: 30.319328};
+var variable;
+
 function initMap() {
-  var uluru = {lat: 59.938955, lng: 30.323115};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: uluru
+    center: variable
   });
   image = 'img/map-pin.png'
   var marker = new google.maps.Marker({
@@ -10,4 +13,10 @@ function initMap() {
     map: map,
     icon: image
   });
+}
+
+if (window.screen.width>=1300) {
+  variable = desktopCenter;
+} else {
+  variable = uluru;
 }
